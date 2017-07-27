8bce4fdca944be5a7c03adae60b3f14fc9317ded
lgybetter <437675103@qq.com>
2017-07-27 13:06:29 +0800

    add the ora

--- bin/zeus.js
+++ bin/zeus.js
@@ -2,6 +2,9 @@
 'use strict';
 const https = require('https')
 const fs = require('fs')
+const cp = require('child_process')
+const rm = require('rimraf').sync
+const ora = require('ora')
 
 const download = (url, dest, cb)  => {
   const file = fs.createWriteStream(dest);
@@ -14,4 +17,20 @@ const download = (url, dest, cb)  => {
   });
 }
 
-download('https://codeload.github.com/lgybetter/yargs-demo/zip/master', 'template.zip')
+const glone = (resp, dest='template') => {
+  const spinner = new Ora({
+    text: 'download template',
+    spinner: process.argv[2]
+  }).start()
+  spinner.color = 'yellow';
+	spinner.text = 'download template';
+  cp.exec(`git clone ${resp} ${dest}`, (err, stdout, stderr) => {
+    if(err) {
+      console.log(err)
+    }
+    spinner.succeed();
+    rm(`${dest}/.git`)
+  })
+}
+
+glone('https://github.com/lgybetter/gyblog-admin.git')
